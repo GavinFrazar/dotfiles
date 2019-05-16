a483da8e448385c2442b5a4bf2733298c4dd6947
Gavin Frazar <gavinfrazar@gmail.com>
2019-05-15 18:47:12 -0700

    Add format keybind and org-table-delete column

--- doom/.doom.d/+bindings.el
+++ doom/.doom.d/+bindings.el
@@ -1,16 +1,23 @@
 ;;; ~/dotfiles/doom/.doom.d/+bindings.el -*- lexical-binding: t; -*-
 
 ;; leader sequences
-(map!
- :leader
- :desc "Eval expression" :g "e" #'eval-expression
- :desc "Toggle comment" :g ";" #'comment-or-uncomment-region
- (:prefix "o"
-   (:desc "org agenda" :prefix "a"
-     :desc "Org clock-in" :g "i" #'org-clock-in))
- (:prefix "q"
-   :desc "kill emacs" :g "k" #'kill-emacs)
- (:prefix "t"
-   :desc "Frame maximized" :g "m" #'toggle-frame-maximized)
- (:prefix "/" :g "c" #'evil-ex-nohighlight)
- (:desc "Repeat last command" :g "r" #'repeat))
+(map! :leader
+      :desc "Eval expression" :g "e" #'eval-expression
+      :desc "Toggle comment" :g ";" #'comment-or-uncomment-region
+      (:prefix "o"
+        (:desc "org agenda" :prefix "a"
+          :desc "Org clock-in" :g "i" #'org-clock-in))
+      (:prefix "q"
+        :desc "kill emacs" :g "k" #'kill-emacs)
+      (:prefix "t"
+        :desc "Frame maximized" :g "m" #'toggle-frame-maximized)
+      (:prefix "/" :g "c" #'evil-ex-nohighlight)
+      (:desc "Repeat last command" :g "r" #'repeat))
+
+(map! :map org-mode-map
+      :localleader
+      (:prefix "b"
+        (:prefix ("d" . "delete")
+          (:desc "Delete table column" :g "c" #'org-table-delete-column))))
+
+(map! [backtab] #'+format/buffer)
