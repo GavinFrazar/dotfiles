db10151808909a3ff99b8d4b9f84f6d16f47ac1e
Gavin Frazar <gavinfrazar@gmail.com>
2021-01-16 23:34:14 -0800

    Add ox-pandoc package

--- doom/dot-doom.d/config.el
+++ doom/dot-doom.d/config.el
@@ -42,5 +42,18 @@
 
 (setq auto-mode-alist (rassq-delete-all 'scheme-mode auto-mode-alist))
 
+(use-package! ox-pandoc
+  :after org)
+
+;; default options for all output formats
+(setq org-pandoc-options '((standalone . _)))
+;; cancel above settings only for 'docx' format
+(setq org-pandoc-options-for-docx '((standalone . nil)))
+;; special settings for beamer-pdf and latex-pdf exporters
+(setq org-pandoc-options-for-beamer-pdf '((pdf-engine . "xelatex")))
+(setq org-pandoc-options-for-latex-pdf '((pdf-engine . "pdflatex")))
+;; special extensions for markdown_github output
+(setq org-pandoc-format-extensions '(markdown_github+pipe_tables+raw_html))
+
 ;; set the theme
 (setq doom-theme 'doom-vibrant)
